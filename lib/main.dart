8e7fb5bed0cc9b82847dd0698e5dedfbca051be5
poplegogdev <poplegogdev@gmail.com>
2020-01-06 21:54:19 -0800

    commit 1

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp (
+      title: 'welcome to my first application',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to my first applicaton'),
+        ),
+        body: Center(
+          child: RandomWords()
+        )
+      )
+    );
+  }
+
+}
+
+class RandomWordState extends State<RandomWords>{
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  @override
+  RandomWordState createState() => new RandomWordState();
+}
